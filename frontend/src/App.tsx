26232284563f51676f2ea93cb9b47ff45e195b0b
Antonio Balbino <abalsantana01@gmail.com>
2022-01-11 00:38:58 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,9 @@
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
-    <header>
-      <nav>
-        <div>
-          <h1>DSMovie</h1>
-          <a href="https://github.com/abalsantana"></a>
-        </div>
-      </nav>
-    </header>
+      <Navbar />
     );
 }
 
