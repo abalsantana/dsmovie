0bc5d56376c750c241d8b788db931f8eb030d78b
abalsantana <abalsantana01@gmail.com>
2022-01-16 13:28:30 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -30,10 +30,13 @@ function Listing() {
             });
     }, [pageNumber]);
 
+    const handlepageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlepageChange}/>
 
             <div className="container">
                 <div className="row">
